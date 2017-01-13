1a0e91092f721efb9053ac69780a5de7d8391d15
Tom Partridge <tom.partridge@nottingham.ac.uk>
2017-01-13 14:08:37 +0000

    Added CompressionService - not used yet.

---
+++ app/src/main/java/uk/ac/nottingham/eaxtp1/CradleRideLogger/CompressionService.java
@@ -0,0 +1,98 @@
+package uk.ac.nottingham.eaxtp1.CradleRideLogger;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.content.Context;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class CompressionService extends IntentService {
+
+    public CompressionService() {
+        super("CompressionService");
+    }
+
+    //    Initialise strings for the zipping
+    String mainPath, folderPath, zipPath;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        mainPath = String.valueOf(getExternalFilesDir(""));
+        folderPath = mainPath + "/New";
+        zipPath = mainPath + "/Zipped";
+
+//        Ensures there's a folder to put the ZIPs in.
+        File zipDirectory = new File(zipPath);
+        if (!zipDirectory.exists()) {
+            zipDirectory.mkdir();
+        }
+
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+        zipFiles(folderPath, zipPath);
+
+    }
+
+    //    Zips all files in the specified folder, into a given folder.
+    public boolean zipFiles(String pathToFolder, String pathToZip) {
+
+        final int BUFFER = 2048;
+
+        File sourceFolder = new File(pathToFolder);
+        int sourceLength = sourceFolder.getParent().length();
+        sourceLength = sourceLength + 4;
+
+        File[] fileList = sourceFolder.listFiles();
+
+        for (File file : fileList) {
+
+            String unmodifiedFilePath = file.getPath();
+            String fileName = unmodifiedFilePath.substring(sourceLength);
+            String fileFolder = fileName.substring(0, fileName.length() - 3) + "zip";
+            pathToZip = pathToZip + fileFolder;
+
+            try {
+
+                FileInputStream fileInputStream = new FileInputStream(unmodifiedFilePath);
+                FileOutputStream zipDestination = new FileOutputStream(pathToZip);
+                ZipOutputStream zipOut = new ZipOutputStream(new BufferedOutputStream(zipDestination));
+                BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream, BUFFER);
+
+                byte data[] = new byte[BUFFER];
+
+                ZipEntry zipEntry = new ZipEntry(fileName);
+                zipOut.putNextEntry(zipEntry);
+
+                int count;
+                while ((count = bufferedInputStream.read(data, 0, BUFFER)) != -1) {
+                    zipOut.write(data, 0, count);
+                }
+
+                file.delete();
+
+                bufferedInputStream.close();
+
+                zipOut.close();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return false;
+            }
+
+        }
+
+        return true;
+    }
+
+}
